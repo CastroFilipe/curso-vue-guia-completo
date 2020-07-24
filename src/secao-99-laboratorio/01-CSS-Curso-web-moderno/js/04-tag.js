853fdc3919f0f9687c7c02f484cdc76a38d55b11
Filipe do Couto Castro <filipe.couto.10@gmail.com>
2020-07-24 00:32:30 -0300

    vídeo aulas extras sobre CSS, do curso web moderno

---
+++ src/secao-99-laboratorio/01-CSS-Curso-web-moderno/js/04-tag.js
@@ -0,0 +1,35 @@
+const cores = {
+    p: '#388e3c',
+    div: '#1565c0',
+    span: '#e53935',
+    seciton: '#f67809',
+    ul: '#5e35b1',
+    ol: '#fbc02d',
+    header: '#e53935',
+    nav: '#e53935',
+    main: '#e53935',
+    footer: '#304ffe',
+    form: '#9f6581',
+    body: '#25b6da',
+    padrao: '#616161',
+    
+    getCorTag(nomeTag) {
+        return this[nomeTag] ? this[nomeTag] : this.padrao
+    }
+
+}
+
+document.querySelectorAll('.tag').forEach(elemento => {
+    const tagName = elemento.tagName.toLocaleLowerCase()
+    console.log(tagName)
+
+    elemento.style.borderColor = cores.getCorTag(tagName)
+
+    if(!elemento.classList.contains('nolabel')){
+        const label = document.createElement('label')
+
+        label.style.backgroundColor = cores.getCorTag(tagName)
+        label.innerHTML = tagName
+        elemento.insertBefore(label, elemento.childNodes[0])
+    }
+})
